db99c886a4453d8e56f8b9befefc077515aa1e48
Daniel Landally <Daniel.Landally@hotmail.fr>
2019-03-22 13:43:38 +0100

    Update Adherent.php

--- Adherent.php
+++ Adherent.php
@@ -1,11 +1,21 @@
 <?php 
 require_once('Manager.php');
+// commentaire TEST
+
+class Adherent extends Personn
+
+
+
+
+
+
+
 
 
 /**
- * 
+ * Model pour connection a la base de donner
  */
-class Adherent extends Manager
+class Adherent1 extends Manager
 {
 
 	public function ajout_adh($prenom, $nom, $email)
@@ -21,29 +31,6 @@ class Adherent extends Manager
 		$db = $this->dbConnect();
 		$recherche = $db->query('SELECT prenom, nom, age, tel, email FROM urc');
 		return $recherche; 
-
 	}
-
-
-
-	/* Exécute une requête préparée en associant des variables PHP 
-$calories = 150;
-$couleur = 'rouge';
-$sth = $dbh->prepare('SELECT nom, couleur, calories
-    FROM fruit
-    WHERE calories < ? AND couleur = ?');
-$sth->bindValue(1, $calories, PDO::PARAM_INT);
-$sth->bindValue(2, $couleur, PDO::PARAM_STR);
-$sth->execute(); 
-
-
-
-$req->bindValue(1, $prenom, PDO::PARAM_STR);
-		$req->bindValue(2, $nom, PDO::PARAM_STR);
-		/*$req->bindValue(3, $age, PDO::PARAM_INT);
-		$req->bindValue(3, $tel, PDO::PARAM_INT);
-		$req->bindValue(4, $email, PDO::PARAM_STR); */
-
-
 
 }
